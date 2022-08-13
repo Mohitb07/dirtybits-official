0eaed48d0d2ac7446ff69cd9048d14f99ecf5460
mohitb07 <bmohit980@gmail.com>
2022-08-13 17:18:18 +0530

    feat(global style): add global styles init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,33 @@
 import '../styles/global.css';
 
+import { MantineProvider } from '@mantine/core';
 import type { AppProps } from 'next/app';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <MantineProvider
+    withGlobalStyles
+    withNormalizeCSS
+    theme={{
+      colorScheme: 'dark',
+      colors: {
+        primary: [
+          '#EFEBFA',
+          '#D3C6F0',
+          '#B7A2E7',
+          '#9B7DDD',
+          '#7F59D4',
+          '#6334CB',
+          '#4F2AA2',
+          '#3B1F7A',
+          '#281551',
+          '#140A29',
+        ],
+      },
+      primaryColor: 'primary',
+    }}
+  >
+    <Component {...pageProps} />
+  </MantineProvider>
 );
 
 export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -42,4 +42,7 @@ module.exports = {
     },
   },
   plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
 };
