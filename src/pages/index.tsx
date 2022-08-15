b0a689df2da9a1bf5888e9c579b381191ab04efb
mohitb07 <bmohit980@gmail.com>
2022-08-15 22:06:40 +0530

    refactor: navbar removed and code refactored

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import FeaturesSection from '@/components/Features';
 import HeroSection from '@/components/Hero';
-import Navbar from '@/components/Navbar';
 import { Meta } from '@/layouts/Meta';
 
 const Index = () => {
@@ -10,10 +9,9 @@ const Index = () => {
         title="DirtyBits"
         description="It's an online coding platform for skill enhancement and students preparing for technical interviews."
       />
-      <div className="relative bg-[#001220]">
+      <div className="bg-[#001220]">
         <div className="elementOne absolute h-[10rem] w-[12rem] md:h-[500px] md:w-[500px]"></div>
         <div className="elementTwo absolute bottom-[15rem] right-[5rem] h-[10rem] w-[12rem] md:bottom-[30rem] md:right-[60rem] md:h-[500px] md:w-[500px]"></div>
-        <Navbar />
         <HeroSection />
         <FeaturesSection />
       </div>
