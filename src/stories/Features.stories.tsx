85195607e4b6df6dcdd2ac8ab8bc692258aabf63
mohitb07 <bmohit980@gmail.com>
2022-08-15 22:02:46 +0530

    feat: added to storybook component

---
+++ src/stories/Features.stories.tsx
@@ -0,0 +1,9 @@
+import FeatureSection from '../components/Features';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: 'Pages/Features',
+  component: FeatureSection,
+};
+
+export const HomePage = () => <FeatureSection />;
---
+++ src/stories/Hero.stories.tsx
@@ -0,0 +1,9 @@
+import HeroSection from '../components/Hero';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  title: 'Pages/Hero',
+  component: HeroSection,
+};
+
+export const HomePage = () => <HeroSection />;
