8eab078cb50e341fa7d4cb44ed4eea8ef141b3d7
mohitb07 <bmohit980@gmail.com>
2022-08-16 23:01:41 +0530

    refactor: cleanup component

--- src/pages/problemset/[query].tsx
+++ src/pages/problemset/[query].tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import PageContainer from '@/layouts/Container';
 import { Meta } from '@/layouts/Meta';
@@ -7,28 +7,6 @@ import { Meta } from '@/layouts/Meta';
 // http://localhost:3000/problemset/all/?status=AC&page=1&topicSlugs=array&difficulty=HARD
 
 const ProblemSet: React.FC = () => {
-  const [data, setData] = useState(null);
-  useEffect(() => {
-    console.log('fetching');
-    fetch('https://jsonplaceholder.typicode.com/todos/')
-      .then((response) => response.json())
-      .then((json) => setData(json));
-  }, []);
-
-  if (!data) {
-    return (
-      <>
-        <Meta
-          title="Loading..."
-          description="It's an online coding platform for skill enhancement and students preparing for technical interviews."
-        />
-        <PageContainer>
-          <div className="text-4xl text-white">Loading...</div>
-        </PageContainer>
-      </>
-    );
-  }
-
   return (
     <>
       <Meta
@@ -36,8 +14,7 @@ const ProblemSet: React.FC = () => {
         description="It's an online coding platform for skill enhancement and students preparing for technical interviews."
       />
       <PageContainer>
-        <div className="text-green-500">Have a good coding</div>
-        <p>{JSON.stringify(data, null, 2)}</p>
+        <div className="text-4xl text-white">Have a good coding</div>
       </PageContainer>
     </>
   );
