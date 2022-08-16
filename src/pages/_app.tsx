bcd696592ddbe18135ecc023a068940a7f459e34
mohitb07 <bmohit980@gmail.com>
2022-08-16 22:45:00 +0530

    feat: mantine theme added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,40 +6,13 @@ import { useRouter } from 'next/router';
 
 import Navbar from '@/components/Navbar';
 import { RouterTransition } from '@/components/RouterTransition';
+import theme from '@/theme/mantine';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const router = useRouter();
   const isHome = router.pathname === '/';
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        colorScheme: 'dark',
-        colors: {
-          primary: [
-            '#EFEBFA',
-            '#D3C6F0',
-            '#B7A2E7',
-            '#9B7DDD',
-            '#7F59D4',
-            '#6334CB',
-            '#4F2AA2',
-            '#3B1F7A',
-            '#281551',
-            '#140A29',
-          ],
-        },
-        primaryColor: 'primary',
-        breakpoints: {
-          xs: 500,
-          sm: 800,
-          md: 1000,
-          lg: 1200,
-          xl: 1400,
-        },
-      }}
-    >
+    <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
       <RouterTransition />
       <Navbar isHome={isHome} activeNav={router.pathname} />
       <Component {...pageProps} />
