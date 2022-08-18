632d2bd0dee9e561d8701ad86e5e290889e39661
mohitb07 <bmohit980@gmail.com>
2022-08-18 22:51:34 +0530

    refactor: utilized reusable multiselect body

--- src/components/MultiSelect/index.tsx
+++ src/components/MultiSelect/index.tsx
@@ -1,109 +1,36 @@
-import { Button, Chip, Divider, Menu, TextInput } from '@mantine/core';
-import { useRouter } from 'next/router';
-import type { MouseEvent } from 'react';
-import { AiOutlineSearch } from 'react-icons/ai';
+import { Button, Menu } from '@mantine/core';
 import { BsChevronDown } from 'react-icons/bs';
-import { useRecoilState } from 'recoil';
 
-import { filterData } from '@/atom/filterData';
+import type { IFilterData } from '@/atom/filterData';
 
-function Demo() {
-  const router = useRouter();
-  const [filterStateData, setFilterStateData] = useRecoilState(filterData);
+import MultiSelectFilter from './CustomBody';
 
-  const itemClick = (
-    e: MouseEvent<HTMLInputElement, globalThis.MouseEvent>
-  ) => {
-    const { value } = e.target as HTMLInputElement;
-    if (filterStateData.tags.includes(value)) {
-      setFilterStateData((prev) => ({
-        ...prev,
-        tags: prev.tags.filter((tag) => tag !== value),
-      }));
-    } else {
-      setFilterStateData((prev) => ({
-        ...prev,
-        tags: [...prev.tags, value],
-      }));
-    }
-    router.push(
-      {
-        pathname: '/problemset/all/',
-        query: {
-          ...router.query,
-          tag: value,
-        },
-      },
-      undefined,
-      { shallow: true }
-    );
-  };
+function Demo({ filterStateData }: { filterStateData: IFilterData }) {
+  const itemList = [
+    'Arrays',
+    'Strings',
+    'Linked-List',
+    'Recursion',
+    'Backtracking',
+    'Trees',
+    'Hash Table',
+  ];
   return (
-    <Menu shadow="md" width={450} offset={15} position="bottom-start" withArrow>
+    <Menu shadow="md" offset={15} position="bottom-start">
       <Menu.Target>
         <Button variant="default" rightIcon={<BsChevronDown />}>
           <span className="text-[#B6C1C4]">Tags</span>
         </Button>
       </Menu.Target>
 
-      <Menu.Dropdown>
-        <div className="p-3">
-          <TextInput
-            variant="filled"
-            icon={<AiOutlineSearch />}
-            placeholder="Filter topics"
-          />
-        </div>
-        <Divider orientation="horizontal" />
-        <div className="space-y-1 px-3 py-1">
-          <Menu.Item disabled>Topics</Menu.Item>
-          <Chip.Group position="left" multiple>
-            <Chip
-              size="xs"
-              color="violet"
-              variant="filled"
-              radius="lg"
-              value="arrays"
-              checked={Boolean(filterStateData.tags.includes('arrays'))}
-              onClick={itemClick}
-            >
-              Arrays
-            </Chip>
-            <Chip
-              size="xs"
-              color="violet"
-              variant="filled"
-              radius="lg"
-              value="string"
-              checked={Boolean(filterStateData.tags.includes('string'))}
-              onClick={itemClick}
-            >
-              Strings
-            </Chip>
-            <Chip
-              size="xs"
-              color="violet"
-              variant="filled"
-              radius="lg"
-              value="linkedlist"
-              checked={Boolean(filterStateData.tags.includes('linkedlist'))}
-              onClick={itemClick}
-            >
-              Linked-List
-            </Chip>
-            <Chip
-              size="xs"
-              color="violet"
-              variant="filled"
-              radius="lg"
-              value="hashtable"
-              checked={Boolean(filterStateData.tags.includes('hashtable'))}
-              onClick={itemClick}
-            >
-              Hash Table
-            </Chip>
-          </Chip.Group>
-        </div>
+      <Menu.Dropdown className="overflow-hidden">
+        <MultiSelectFilter
+          filterStateData={filterStateData}
+          itemList={itemList}
+          className="w-[300px] md:w-[400px]"
+          title="Topics"
+          placeholder="Filter topics"
+        />
       </Menu.Dropdown>
     </Menu>
   );
