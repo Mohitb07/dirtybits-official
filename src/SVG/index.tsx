490111b7f7b6d5e6423328a0c27c779a9e68f43f
mohitb07 <bmohit980@gmail.com>
2022-08-18 22:46:39 +0530

    feat: svg added

---
+++ src/SVG/index.tsx
@@ -0,0 +1,18 @@
+export default function TickIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="1em"
+      height="1em"
+      fill="currentColor"
+      className="h-[18px] w-[18px] text-green-500"
+    >
+      <path
+        fillRule="evenodd"
+        d="M9.688 15.898l-3.98-3.98a1 1 0 00-1.415 1.414L8.98 18.02a1 1 0 001.415 0L20.707 7.707a1 1 0 00-1.414-1.414l-9.605 9.605z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  );
+}
