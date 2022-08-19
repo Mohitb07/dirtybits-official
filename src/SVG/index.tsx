86804b735d09f07f015ee9e004f5921ed88e28d7
mohitb07 <bmohit980@gmail.com>
2022-08-19 22:23:07 +0530

    refactor: default to named export

--- src/SVG/index.tsx
+++ src/SVG/index.tsx
@@ -1,4 +1,4 @@
-export default function TickIcon() {
+export const TickIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -15,4 +15,4 @@ export default function TickIcon() {
       ></path>
     </svg>
   );
-}
+};
