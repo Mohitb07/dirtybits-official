619564680e4ccebd92ce4261bc8c62366c7796ee
mohitb07 <bmohit980@gmail.com>
2022-08-19 22:25:18 +0530

    feat: table row component

---
+++ src/components/Table/Row/index.tsx
@@ -0,0 +1,69 @@
+import Link from 'next/link';
+import React from 'react';
+
+import { TickIcon } from '@/SVG';
+
+type RowProps = {
+  problemId: number;
+  problemTitle: string;
+  difficulty: string;
+  status: string;
+};
+
+const Row: React.FC<RowProps> = ({
+  problemId,
+  problemTitle,
+  difficulty,
+  status,
+}) => {
+  let difficultyColor;
+  let statusSVG;
+  switch (status) {
+    case 'completed':
+      statusSVG = <TickIcon />;
+      break;
+    case 'attempted':
+      statusSVG = <TickIcon />;
+      break;
+    case 'not attempted':
+      statusSVG = <TickIcon />;
+      break;
+    default:
+      throw new Error(`Unknown status: ${status}`);
+  }
+  switch (difficulty) {
+    case 'Easy':
+      difficultyColor = 'text-green-500';
+      break;
+    case 'Medium':
+      difficultyColor = 'text-yellow-500';
+      break;
+    case 'Hard':
+      difficultyColor = 'text-red-500';
+      break;
+    default:
+      throw new Error(`Unknown status: ${status}`);
+  }
+  return (
+    <tr>
+      <td className="table-data border-b border-slate-800 text-white">
+        <Link href="/">
+          <a className="tracking-wide text-stone-200 no-underline transition-all duration-200 ease-in hover:text-indigo-400 md:text-sm">
+            {problemId}. {problemTitle}
+          </a>
+        </Link>
+      </td>
+      <td className="table-data py-4 px-6">
+        <p
+          className={`rounded py-1 px-3 text-xs font-bold tracking-wider ${difficultyColor}`}
+        >
+          {difficulty}
+        </p>
+      </td>
+      <td className="table-data cursor-default border-b border-slate-800 text-lg">
+        <span className={`px-3  font-light tracking-wider`}>{statusSVG}</span>
+      </td>
+    </tr>
+  );
+};
+export default Row;
