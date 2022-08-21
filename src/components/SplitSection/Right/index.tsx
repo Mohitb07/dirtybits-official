cd7f6d1ae21cd4ce94bd11f92ff673c848f8e7dc
mohitb07 <bmohit980@gmail.com>
2022-08-21 22:12:19 +0530

    feat: problem solving right pane init

---
+++ src/components/SplitSection/Right/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+type RightPaneProps = {};
+
+const RightPane: React.FC<RightPaneProps> = () => {
+  return (
+    <div className="h-[100vh] p-5">
+      <h1 className="text-2xl ">Right Pane</h1>
+    </div>
+  );
+};
+export default RightPane;
