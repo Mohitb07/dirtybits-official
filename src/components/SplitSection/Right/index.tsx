f1e6d92ce75fadad371b4319f3fc0feac0860e10
mohitb07 <bmohit980@gmail.com>
2022-08-22 22:09:48 +0530

    refactor: language, theme, code editor support added

--- src/components/SplitSection/Right/index.tsx
+++ src/components/SplitSection/Right/index.tsx
@@ -1,11 +1,74 @@
-import React from 'react';
+import { Button } from '@mantine/core';
+import React, { useEffect, useRef, useState } from 'react';
+
+import CodeEditor from '@/components/CodeEditor';
+import { languageOptions } from '@/constants/languageOptions';
+import { defineTheme } from '@/utils/defineTheme';
+
+import LanguageDropdown from './Language';
+import ThemeDropdown from './Theme';
 
 type RightPaneProps = {};
 
+export interface IEditorTheme {
+  value: string;
+  label: string;
+}
+
 const RightPane: React.FC<RightPaneProps> = () => {
+  const [theme, setTheme] = React.useState<IEditorTheme>({
+    value: '',
+    label: '',
+  });
+  const codeValue = useRef('');
+
+  const onEditorChange = (code: any) => {
+    codeValue.current = code;
+  };
+
+  const [language, setLanguage] = useState(languageOptions[0]);
+
+  const onSelectChange = (lang: any) => {
+    console.log('selected Option...', lang);
+    setLanguage(lang);
+  };
+
+  useEffect(() => {
+    defineTheme('night-owl').then((_) =>
+      setTheme({ value: 'night-owl', label: 'night-owl' })
+    );
+  }, []);
+
+  const themeChangeHandler = (e: any) => {
+    defineTheme(e).then((_) => {
+      setTheme({ value: e, label: e });
+    });
+  };
+
   return (
     <div className="h-[100vh] p-5">
-      <h1 className="text-2xl ">Right Pane</h1>
+      <div className="mb-10 flex items-center gap-10">
+        <LanguageDropdown onSelectChange={onSelectChange} />
+        <ThemeDropdown onThemeChange={themeChangeHandler} theme={theme.value} />
+      </div>
+      <CodeEditor
+        theme={theme.value}
+        language={language?.value!}
+        onEditorChange={onEditorChange}
+        code={codeValue}
+      />
+      <div className="my-10 flex items-center justify-end space-x-3">
+        <div className="btnRing p-1">
+          <Button variant="white" className=" text-black" radius="xl" size="md">
+            Run
+          </Button>
+        </div>
+        <div className="btnRing p-1">
+          <Button variant="white" className=" text-black" radius="xl" size="md">
+            Submit
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
