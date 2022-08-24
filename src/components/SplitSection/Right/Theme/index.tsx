9df868c973a9941f5651e4ff3e39a7faba803573
mohitb07 <bmohit980@gmail.com>
2022-08-24 22:49:29 +0530

    refactor: custom right section

--- src/components/SplitSection/Right/Theme/index.tsx
+++ src/components/SplitSection/Right/Theme/index.tsx
@@ -1,6 +1,7 @@
 import { Select } from '@mantine/core';
 import monacoThemes from 'monaco-themes/themes/themelist.json';
 import React from 'react';
+import { HiChevronDown } from 'react-icons/hi';
 
 type ThemeProps = {
   onThemeChange: (theme: string) => void;
@@ -10,6 +11,7 @@ type ThemeProps = {
 const ThemeDropdown: React.FC<ThemeProps> = ({ onThemeChange, theme }) => {
   return (
     <Select
+      rightSection={<HiChevronDown />}
       label="Theme"
       placeholder="Pick one"
       data={Object.entries(monacoThemes).map(([themeId, themeName]) => ({
