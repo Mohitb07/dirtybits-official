5ac2d69c25618c1bb71618cd8add0f3754f72711
mohitb07 <bmohit980@gmail.com>
2022-08-26 22:14:08 +0530

    feat: nprogress setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,27 @@
 import '../styles/global.css';
+import '../styles/Nprogress.css';
 
 import { MantineProvider } from '@mantine/core';
 import type { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
+import Router, { useRouter } from 'next/router';
+import NProgress from 'nprogress';
 import { RecoilRoot } from 'recoil';
 
 import Navbar from '@/components/Navbar';
-import { RouterTransition } from '@/components/RouterTransition';
 import AuthUserProvider from '@/context/authUser';
 import theme from '@/theme/mantine';
+
+NProgress.configure({ showSpinner: false });
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const router = useRouter();
   const isHome = router.pathname === '/';
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
-      <RouterTransition />
       <RecoilRoot>
         <AuthUserProvider>
           <Navbar isHome={isHome} activeNav={router.pathname} />
